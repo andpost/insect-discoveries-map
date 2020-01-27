d289011821dbf5901d293ae439eb1e080347a0a2
andpost <andpost79@gmail.com>
2020-01-27 15:02:40 +0100

    Added impressum page (no content so far)

--- discoveries_map/src/app/app-routing.module.ts
+++ discoveries_map/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexPageComponent } from './index-page/index-page.component';
 import { MapPageComponent } from './map-page/map-page.component';
 import { SpeciesPageComponent } from './species-page/species-page.component';
+import { ImpressumPageComponent } from './impressum-page/impressum-page.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'arten',
     component: SpeciesPageComponent
+  },
+  {
+    path: 'impressum',
+    component: ImpressumPageComponent
   },
   {
     path: '',
--- discoveries_map/src/app/app.module.ts
+++ discoveries_map/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { LightboxModule } from 'ngx-lightbox';
 import { SpeciesPageComponent } from './species-page/species-page.component';
 import { MapPageComponent } from './map-page/map-page.component';
 import { IndexPageComponent } from './index-page/index-page.component';
+import { ImpressumPageComponent } from './impressum-page/impressum-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapPageComponent,
     SpeciesPageComponent,
-    IndexPageComponent
+    IndexPageComponent,
+    ImpressumPageComponent
   ],
   imports: [
     BrowserModule,
