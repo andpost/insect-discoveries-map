222199cf040fedac8f879c0d2db83afc1f1e5a4d
andpost <andpost79@gmail.com>
2020-01-28 16:13:55 +0100

    Initial version of discoveries-page

--- discoveries_map/src/app/discoveries-page/discoveries-page.component.ts
+++ discoveries_map/src/app/discoveries-page/discoveries-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Beobachtung } from "../beobachtung-entity";
+import { DataService } from '../app.dataservice';
 
 @Component({
   selector: 'app-discoveries-page',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoveriesPageComponent implements OnInit {
 
-  constructor() { }
+  beobachtungen : Beobachtung[];
+
+  constructor(private dataService: DataService) { 
+    this.loadDiscoveries();
+  }
 
   ngOnInit() {
+  }
+
+  loadDiscoveries() {
+    this.dataService.getInsektenFunde().subscribe((data: Beobachtung[]) => this.refreshData(data));
+  }
+
+  refreshData(beobachtungen: Beobachtung[]) {
+    this.beobachtungen = [];
+
+    beobachtungen.forEach(beobachtung => {
+      beobachtung.datumFormattiert = this.dataService.formatDateString(beobachtung.datum);
+
+      this.beobachtungen.push(beobachtung);
+    });
+
+    this.beobachtungen.sort((a, b) => {
+      if (a.datum < b.datum) {
+        return -1;
+      } else if (a.datum > b.datum) {
+        return 1;
+      }
+      return 0;
+    });
   }
 
 }
